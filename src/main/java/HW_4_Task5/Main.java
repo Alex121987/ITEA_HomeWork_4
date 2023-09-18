dd149574a866f163289d761ac6e8db63f07c6010
Alex Grigoriev <alexander.grigoriev.121987@gmail.com>
2023-09-18 09:10:58 +0300

    Fifth task.

---
+++ src/main/java/HW_4_Task5/Main.java
@@ -0,0 +1,12 @@
+package HW_4_Task5;
+
+/**
+ * Створити клас MyArea, в ньому оголосити константу PI = 3.14 та статичний метод areaOfCircle,
+ * який повинен приймати радіус та використовуючи PI порахувати площу кола і виводить результат в консоль.
+ * Створити клас Main, де запустити цей метод.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(MyArea.areaOfCircle(5));
+    }
+}
---
+++ src/main/java/HW_4_Task5/MyArea.java
@@ -0,0 +1,10 @@
+package HW_4_Task5;
+
+public class MyArea {
+    final static double PI = 3.14;
+
+    public static double areaOfCircle(double radius) {
+        return PI * radius * radius;
+    }
+
+}
