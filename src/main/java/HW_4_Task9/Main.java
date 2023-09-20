67ad3fb31bf13a0f48d0084d7ddbca8f5e835829
Alex Grigoriev <alexander.grigoriev.121987@gmail.com>
2023-09-20 11:02:56 +0300

    Ninth task with typecasting.

--- src/main/java/HW_4_Task9/Main.java
+++ src/main/java/HW_4_Task9/Main.java
@@ -14,21 +14,22 @@ public class Main {
     static int counter = 0;
     public static final String RESET = "\u001B[0m";
     public static final String[] COLORS = {"\u001B[30m", "\u001B[31m", "\u001B[32m", "\u001B[33m",
-            "\u001B[34m",  "\u001B[35m", "\u001B[36m", "\u001B[37m"};
-    // System.out.println(ANSI_RED + "This text is red!" + ANSI_RESET);
+                                           "\u001B[34m",  "\u001B[35m", "\u001B[36m", "\u001B[37m"};
 
     public static void main(String[] args) {
-        Printer printer = new Printer();
-        LaserPrinter laserPrinter1 = new LaserPrinter();
+        Printer printer1 = new Printer();
+        Printer printer2 = new Printer();
+        Printer laserPrinter1 = new LaserPrinter();
         MatrixPrinter matrixPrinter1 = new MatrixPrinter();
         LaserPrinter laserPrinter2 = new LaserPrinter();
-        MatrixPrinter matrixPrinter2 = new MatrixPrinter();
+        Printer matrixPrinter2 = new MatrixPrinter();
 
-        printer.print("printer");
+        printer1.print("printer1");
         laserPrinter1.print("laser printer1");
         matrixPrinter1.print("matrix printer1");
         laserPrinter2.print("laser printer2");
         matrixPrinter2.print("matrix printer2");
+        printer2.print("printer2");
 
     }
 }
